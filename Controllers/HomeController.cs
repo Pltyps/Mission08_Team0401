d08fea3bc276982c95a277728e56246f6dfa79fd
Pltyps <166666186+Pltyps@users.noreply.github.com>
2025-02-28 01:24:57 -0700

    Finished Quadrant View with Update and Delete Buttons, Navbar update with 2 views. Remove Index.cshtml and its elements as needed. Cleared unnecessary files from the project directory. Added bare minimum css styling for the quadrants.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,18 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Mission08_Team0401.Models;
+//using Mission08_Team0401.Models;
 
 namespace Mission08_Team0401.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult Quadrants()
     {
         return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
